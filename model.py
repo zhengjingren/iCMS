fabbda60dfdd2f33a4dc60dcf1e4532cea47591e
LiuXingChao <923863800@qq.com>
2012-12-23 16:42:45 +0800

    hehe

---
+++ model.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from sqlalchemy import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+if __name__ == '__main__':
+    app.run()
